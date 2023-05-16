8ebc04470d7b329ad88cb67917cf906b34d6bcfd
hangnt.vmo <hangnt3@vmodev.com>
2023-05-16 17:45:46 +0700

    feat: home page

--- src/components/pages/Home/ModalWelcome.js
+++ src/components/pages/Home/ModalWelcome.js
@@ -1,12 +1,23 @@
 import Modal from '~/components/common/Modal';
 import Button from '~/components/common/Button';
+import Input from '~/components/common/Input';
+import { useForm } from 'react-hook-form';
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
 function ModalWelcome({ data }) {
+  const { t } = useTranslation(['auth', 'message']);
+
   const [isShowModal, setIsShowModal] = useState(!JSON.parse(localStorage.getItem('show-modal-welcome')));
   const handleClose = () => {
     setIsShowModal(false);
     localStorage.setItem('show-modal-welcome', true);
   };
+  const form = useForm({
+    defaultValues: {
+      email: '',
+    },
+  });
   return (
     <div>
       <Modal visible={isShowModal} close={handleClose}>
@@ -19,6 +30,8 @@ function ModalWelcome({ data }) {
               <div className="text-center font-bold pb-1.5">{data?.content?.subtitle}</div>
               <div className="text-center font-bold pb-1 text-red">{data?.content?.title}</div>
               <div className="text-center pb-1.5 text-red">On your first order </div>
+              <Input name="email" placeholder={t('email')} type="text" form={form} />
+
               <Button className={`bg-black hover:bg-black`}>
                 <div className="py-0.5 text-white ">Shop Now</div>
               </Button>
